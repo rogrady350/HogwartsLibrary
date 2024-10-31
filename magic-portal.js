81a8f56ecc38590efdc4e5ac495217854b2fa650
Robert OGrady <turnstoashes350@gmail.com>
2024-10-31 14:36:44 -0400

    added functions

--- magic-portal.js
+++ magic-portal.js
@@ -5,13 +5,13 @@ function getSpells() {
         url: portalUrl + "/get-spells",
         type: "get",
         success: function(response) {
-            response = JSON.parse(response);
-            showTable(response.spells); // Pass spells array to showTable
+            data = JSON.parse(response);
+            showTable(data.spells); // Pass spells array to showTable
             
         },
         error: function(response) {
-            response = JSON.parse(response);
-            console.error(response.msg);
+            data = JSON.parse(response);
+            console.error(data.msg);
          }
     });
 }
@@ -25,7 +25,7 @@ function showTable(spellsTable) {
             htmlString += "<td>" + spell.name + "</td>";
             htmlString += "<td>" + spell.type + "</td>";
             htmlString += "<td>" + spell.effect + "</td>";
-            htmlString += "<td>" + (spell["counter-spell"] || "N/A") + "</td>";
+            htmlString += "<td>" + (spell["counter-spell"] || " ") + "</td>";
             htmlString += "<td><button>Delete</button> <button>Edit</button></td>"; //place holder buttons. not functiong at this time
         htmlString += "</tr>";
     });
@@ -34,3 +34,85 @@ function showTable(spellsTable) {
 }
 
 getSpells();   //populate table on load
+
+
+//add new spells to library 
+function addSpell() {
+    $.ajax({
+        url: portalUrl + "/add-spells",
+        type: "post",
+        data: JSON.stringify(),
+        success: function(response) {
+            data = JSON.parse(response.msg);
+            
+        },
+        error: function(response) {
+            response = JSON.parse(response);
+            console.error(response.msg);
+         }
+    });
+}
+
+function getSpellsByType() {
+    $.ajax({
+        url: portalUrl + "/add-spells",
+        type: "post",
+        success: function(response) {
+            response = JSON.parse(response);
+            showTable(response.spells); // Pass spells array to showTable
+            
+        },
+        error: function(response) {
+            response = JSON.parse(response);
+            console.error(response.msg);
+         }
+    });
+}
+
+function deleteSpell() {
+    $.ajax({
+        url: portalUrl + "/add-spells",
+        type: "post",
+        success: function(response) {
+            response = JSON.parse(response);
+            showTable(response.spells); // Pass spells array to showTable
+            
+        },
+        error: function(response) {
+            response = JSON.parse(response);
+            console.error(response.msg);
+         }
+    });
+}
+
+function updateSpell() {
+    $.ajax({
+        url: portalUrl + "/add-spells",
+        type: "post",
+        success: function(response) {
+            response = JSON.parse(response);
+            showTable(response.spells); // Pass spells array to showTable
+            
+        },
+        error: function(response) {
+            response = JSON.parse(response);
+            console.error(response.msg);
+         }
+    });
+}
+
+function spellOfTheDay() {
+    $.ajax({
+        url: portalUrl + "/add-spells",
+        type: "post",
+        success: function(response) {
+            response = JSON.parse(response);
+            showTable(response.spells); // Pass spells array to showTable
+            
+        },
+        error: function(response) {
+            response = JSON.parse(response);
+            console.error(response.msg);
+         }
+    });
+}
